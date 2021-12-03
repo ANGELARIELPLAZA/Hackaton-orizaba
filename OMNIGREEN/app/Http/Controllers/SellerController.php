643d60cba4f4ee5ec054605e5cb93fdec6d843f0
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2021-12-03 11:08:42 -0600

    app
    APLICACION WEB-APP PARA LOGISTICA OMNIGREEN

---
+++ OMNIGREEN/app/Http/Controllers/SellerController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Seller;
+use Illuminate\Http\Request;
+
+class SellerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Seller::latest()->paginate(5);
+    
+        return view('sellers.index',compact('data'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('sellers.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required',
+            'tipoUsuario' => 'required',
+            'material' => 'required',
+            'recepcion' => 'required',
+            'peso' => 'required',
+   ]);
+    
+        Seller::create($request->all());
+     
+        return redirect()->route('sellers.index')
+                        ->with('success','Post created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Seller $seller)
+    {
+        return view('sellers.show',compact('seller'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Seller $seller)
+    {
+        return view('sellers.edit',compact('seller'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Seller $seller)
+    {
+        $request->validate([
+            'tipoUsuario' => 'required',
+            'material' => 'required',
+            'peso' => 'required',
+            'pago' => 'required',
+            'empresa' => 'nullable',
+            'direccion' => 'nullable'
+        ]);
+    
+        $seller->update($request->all());
+    
+        return redirect()->route('sellers.index')
+                        ->with('success','Post updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Seller $seller)
+    {
+        $seller->delete();
+    
+        return redirect()->route('sellers.index')
+                        ->with('success','Post deleted successfully');
+    }
+}
